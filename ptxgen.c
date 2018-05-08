f253ff750f465ba5775be7c7df9079ab4981b83c
rpalamut <rpalamut@dawn7.stanford.edu>
2018-05-08 09:05:57 -0700

    adding ptx gen

---
+++ ptxgen.c
@@ -0,0 +1,148 @@
+#include <math.h>
+#include <cuda.h>
+#include "drvapi_error_string.h"
+#include "nvvm.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+// This will output the proper CUDA error strings in the event that a CUDA host call returns an error
+#define checkCudaErrors(err)  __checkCudaErrors (err, __FILE__, __LINE__)
+
+// These are the inline versions for all of the SDK helper functions
+void __checkCudaErrors( CUresult err, const char *file, const int line )
+{
+    if( CUDA_SUCCESS != err) {
+        fprintf(stderr, "checkCudaErrors() Driver API error = %04d \"%s\" from file <%s>, line %i.\n",
+                err, getCudaDrvErrorString(err), file, line );
+        exit(-1);
+    }
+}
+
+CUdevice cudaDeviceInit()
+{
+    CUdevice cuDevice = 0;
+    int deviceCount = 0;
+    CUresult err = cuInit(0);
+    char name[100];
+    int major=0, minor=0;
+
+    if (CUDA_SUCCESS == err)
+        checkCudaErrors(cuDeviceGetCount(&deviceCount));
+    if (deviceCount == 0) {
+        fprintf(stderr, "cudaDeviceInit error: no devices supporting CUDA\n");
+        exit(-1);
+    }
+    checkCudaErrors(cuDeviceGet(&cuDevice, 0));
+    cuDeviceGetName(name, 100, cuDevice);
+    printf("Using CUDA Device [0]: %s\n", name);
+
+    checkCudaErrors( cuDeviceComputeCapability(&major, &minor, cuDevice) );
+    if (major < 2) {
+        fprintf(stderr, "Device 0 is not sm_20 or later\n");
+        exit(-1);
+    }
+    return cuDevice;
+}
+
+
+CUresult initCUDA(CUcontext *phContext,
+                  CUdevice *phDevice,
+                  CUmodule *phModule,
+                  CUfunction *phKernel,
+                  const char *ptx)
+{
+    // Initialize 
+    *phDevice = cudaDeviceInit();
+
+    // Create context on the device
+    checkCudaErrors(cuCtxCreate(phContext, 0, *phDevice));
+
+    // Load the PTX 
+    checkCudaErrors(cuModuleLoadDataEx(phModule, ptx, 0, 0, 0));
+
+    // Locate the kernel entry poin
+    checkCudaErrors(cuModuleGetFunction(phKernel, *phModule, "simple"));
+
+    return CUDA_SUCCESS;
+}
+
+char *loadProgramSource(const char *filename, size_t *size) 
+{
+    struct stat statbuf;
+    FILE *fh;
+    char *source = NULL;
+    *size = 0;
+    fh = fopen(filename, "rb");
+    if (fh) {
+        stat(filename, &statbuf);
+        source = (char *) malloc(statbuf.st_size+1);
+        if (source) {
+            fread(source, statbuf.st_size, 1, fh);
+            source[statbuf.st_size] = 0;
+            *size = statbuf.st_size+1;
+        }
+    }
+    else {
+        fprintf(stderr, "Error reading file %s\n", filename);
+        exit(-1);
+    }
+    return source;
+}
+
+extern "C" char *generatePTX(const char *ll, size_t size, const char *filename);
+extern "C" char *generatePTX(const char *ll, size_t size, const char *filename)
+{
+    nvvmResult result;
+    nvvmProgram program;
+    size_t PTXSize;
+    char *PTX = NULL;
+
+    result = nvvmCreateProgram(&program);
+    if (result != NVVM_SUCCESS) {
+        fprintf(stderr, "nvvmCreateProgram: Failed\n");
+        exit(-1); 
+    }
+
+    result = nvvmAddModuleToProgram(program, ll, size, filename);
+    if (result != NVVM_SUCCESS) {
+        fprintf(stderr, "nvvmAddModuleToProgram: Failed\n");
+        exit(-1);
+    }
+ 
+    result = nvvmCompileProgram(program,  0, NULL);
+    if (result != NVVM_SUCCESS) {
+        char *Msg = NULL;
+        size_t LogSize;
+        fprintf(stderr, "nvvmCompileProgram: Failed\n");
+        nvvmGetProgramLogSize(program, &LogSize);
+        Msg = (char*)malloc(LogSize);
+        nvvmGetProgramLog(program, Msg);
+        fprintf(stderr, "%s\n", Msg);
+        free(Msg);
+        exit(-1);
+    }
+    
+    result = nvvmGetCompiledResultSize(program, &PTXSize);
+    if (result != NVVM_SUCCESS) {
+        fprintf(stderr, "nvvmGetCompiledResultSize: Failed\n");
+        exit(-1);
+    }
+    
+    PTX = (char*)malloc(PTXSize);
+    result = nvvmGetCompiledResult(program, PTX);
+    if (result != NVVM_SUCCESS) {
+        fprintf(stderr, "nvvmGetCompiledResult: Failed\n");
+        free(PTX);
+        exit(-1);
+    }
+    
+    result = nvvmDestroyProgram(&program);
+    if (result != NVVM_SUCCESS) {
+      fprintf(stderr, "nvvmDestroyProgram: Failed\n");
+      free(PTX);
+      exit(-1);
+    }
+    
+    return PTX;
+}
